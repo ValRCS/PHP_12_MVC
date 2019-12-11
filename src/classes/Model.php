4378a34f4da9f501c2750afd9555dc9747662bcd
Valdis Saules <valdis.s.coding@gmail.com>
2019-12-11 12:04:21 +0200

    Added DB connection

--- src/classes/Model.php
+++ src/classes/Model.php
@@ -1,12 +1,35 @@
 <?php
+require_once __DIR__ . "/../../config/config.php";
 class Model
 {
-    public $string; // this is our DB for now
+    private $db;
     private $view;
     public function __construct($view)
     {
         $this->view = $view;
-        $this->string = "MVC + PHP = Awesome!";
+        $this->db = $this->createDB(CFG);
+    }
+
+    private function createDB($cfg)
+    {
+        $server = $cfg['server'];
+        $db = $cfg['db'];
+        $user = $cfg['user'];
+        $pw = $cfg['pw'];
+
+        try {
+            //if we needed to set port it would come after $SERVER;port=3307;dbname=$DB
+            $conn = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pw);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            //we would deal with broken connection here
+            echo "Connection failed: " . $e->getMessage();
+            $conn = null;
+        }
+
+        return $conn;
+
     }
 
     public function processData($incoming = null)
--- src/classes/View.php
+++ src/classes/View.php
@@ -20,7 +20,27 @@ class View
 
     private function getHeader()
     {
-        return "";
+        $pagetitle = "My music page"; //TODO get title from config
+        $stylefile = ""; //TODO get style from config
+        $jsfile = "";
+        $html = <<<MYLIMITER
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>$pagetitle</title>
+<link rel="stylesheet" href="styles/$stylefile">
+<script
+src="https://code.jquery.com/jquery-3.4.1.min.js"
+integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+crossorigin="anonymous"></script>
+<script src="scripts/$jsfile" defer></script>
+</head>
+<body>
+MYLIMITER;
+        return $html;
     }
 
     private function getMain($data)
@@ -30,6 +50,10 @@ class View
 
     private function getFooter()
     {
-        return "";
+        $year = date(DATE_RFC2822); //TODO get year dynamically
+        $html = "<footer>(C)$year</footer>";
+        $html .= "</body>";
+        $html .= "</html>";
+        return $html;
     }
 }
